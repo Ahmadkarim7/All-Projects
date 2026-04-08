4efd9d10de82c2c041673f2e9e1db906721d4f74
Ahmadkarim7 <158099418+Ahmadkarim7@users.noreply.github.com>
2026-04-07 22:07:25 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -52,3 +52,11 @@ const revealElements = document.querySelectorAll('.reveal');
   revealElements.forEach((el) => {
     observer.observe(el);
   });
+
+  //Menu Bar
+ const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav');
+
+menuToggle.addEventListener('click', function () {
+    nav.classList.toggle('active');
+});
